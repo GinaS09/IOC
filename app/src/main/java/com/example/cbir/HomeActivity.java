635d039b482e0a778674a804772584bae739f90e
Georgian02 <georgian.anghelescu99@gmail.com>
2021-05-05 12:43:14 +0300

    home

--- app/src/main/java/com/example/cbir/HomeActivity.java
+++ app/src/main/java/com/example/cbir/HomeActivity.java
@@ -1,35 +1,53 @@
 package com.example.cbir;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.example.cbir.adapters.HomeAdapter;
 
 public class HomeActivity extends AppCompatActivity {
+    ListView list;
+
+    String[] maintitle ={
+            "Title 1","Title 2",
+            "Title 3","Title 4",
+            "Title 5",
+    };
+
+    String[] descriere ={
+            "Sub Title 1","Sub Title 2",
+            "Sub Title 3","Sub Title 4",
+            "Sub Title 5",
+    };
+
+    Integer[] imgid={
+            R.drawable.ro,R.drawable.ro,
+            R.drawable.ro,R.drawable.ro,
+            R.drawable.ro,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        List<String> elemente=new ArrayList<>();
-        elemente.add("Elementul 1");
-        elemente.add("Elementul 2");
-        elemente.add("Elementul 3");
-        elemente.add("Elementul 4");
+//        List<String> elemente=new ArrayList<>();
+//        elemente.add("Elementul 1");
+//        elemente.add("Elementul 2");
+//        elemente.add("Elementul 3");
+//        elemente.add("Elementul 4");
+//
+//        ArrayAdapter<String> adapter=new ArrayAdapter<>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                elemente);
+//        ListView lv=findViewById(R.id.listView);
+//        lv.setAdapter(adapter);
 
-        ArrayAdapter<String> adapter=new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                elemente);
-        ListView lv=findViewById(R.id.listView);
-        lv.setAdapter(adapter);
+        HomeAdapter adapter=new HomeAdapter(this, maintitle,descriere,imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
     }
 }
